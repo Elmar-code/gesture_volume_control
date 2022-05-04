eb49fdc5a0ab75ac7aeb71e461c8088508b45a65
Elmar-code <104902722+Elmar-code@users.noreply.github.com>
2022-05-04 17:37:54 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,123 @@
+# import random, math
+# lower=int(input("boshlangich rakam\n"))
+# upper=int(input("yakuniy rakam\n"))
+# x=random.randint(lower,upper)
+# print("\n\t sizda imkoniyatlar=",round(math.log(upper-lower+1,2)))
+# count=0
+# while count<math.log(upper-lower+1,2):
+#     count +=1
+#     guess=int(input("taxmin qigan sonizi kiritin"))
+#     if x==guess:
+#         print("siz buni",count,"urinishda bajardingiz")
+#         break
+#     elif x<guess:
+#         print("biroz kotarin")
+#     elif x<guess:
+#         print("biroz tushin")
+# if count>=math.log(upper-lower-1,2):
+#     print(f"\n men oylagan son {x} edi")
+#     print(f"\n salomat bolin")
+# input('kalkulyator\n')
+# a=int(input())
+# b=int(input())
+# d=a+b
+# print(d)
+# import random
+# number=random.randint(1,2)
+# for i in range(3):
+#     a=int(input('oylagan sonizi kiritin\n'))
+#     if a<number:
+#         print("kattarok soni kiritin iltimos")
+#     elif a>number:
+#         print("kichikrok soni kiritin iltimos")
+#     elif a==number:
+#         print("siz yutingiz"),print("salomat bolin")
+#         break
+#     else:
+#         print("siz yutkazdingiz")
+#one line
+# a=330
+# b=330
+# print("a") if a>b else print("=")if a==b else print("b")
+# print("togri") if input("ism kiritin\n")=="elmar" else print("notogri")
+# a=input("ism kiritn\n")
+# if a=="islom":
+#     print("togri")
+# else:
+#     print("notogri")
+# if a>b:
+#     print("a")
+# elif a==b:
+#     print("=")
+# else:
+#     print("b")
+#a=input("pasport seriyangiz\n")
+#print() if a[0:1].isupper()and a[2:8].isdigit() else print("notogri")
+# a=int(input("rakam kiritin 100 dan kam soni riritin\n"))
+# print(a+10) if a in range(100) else print("siz notogri rakam kiritiz")
+#14.02
+# import random
+# a=random.randint(1000,9999)
+# print(a)
+# from random import randint
+# a=randint(0,9)
+# b=randint(0,9)
+# s=randint(0,9)
+# f=randint(0,9)
+# print(f"sizni parolingiz{a}{b}{s}{f}")
+# import random
+# number=random.randint(0, 20)
+# for i in range(2):
+#     a=int(input('oylagan sonizi kiritin\n'))
+#     if a<number:
+#         print(False)
+#     elif a>number:
+#         print(False)
+#     elif a==number:
+#         print(True)
+#         break
+#     else:
+#         print("siz yutrazdingiz")
+# from random import randint
+# a0,a1,a2,a3,a4,a5 = [],randint(0,100),randint(0,100),randint(0,100),randint(0,100),randint(0,100)
+# a6,a7,a8,a9,a10 = randint(0,100),randint(0,100),randint(0,100),randint(0,100),randint(0,100)
+# a0.append(a1)
+# a0.append(a2)
+# a0.append(a3)
+# a0.append(a4)
+# a0.append(a5)
+# a0.append(a6)
+# a0.append(a7)
+# a0.append(a8)
+# a0.append(a9)
+# a0.append(a10)
+# b=input("taxmin kilin\n")
+# if b in a0:
+#     print(True)
+# elif b not in a0:
+#     print(False)
+# c=input("yana taxmi qiling\n")
+# if c in a0:
+#     print(True)
+#     print(a0)
+# elif c not in a0:
+#     print(False)
+#     print(a0)
+# a=int(input("son kiritin\n"))
+# b=int(input("son kiritin\n"))
+# s=a+b
+# print(s)
+#uyga vazifa
+# a=int(input('son kiritin\n'))
+# if a % 2==0:
+#     print('juft son\n')
+# else:
+#     print('toq son\n')
+
+
+
+
+
+
+
+
